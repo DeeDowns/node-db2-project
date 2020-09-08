71c60bd2e4bb06b6ce8cfa68371537684da8a2cd
DeeDowns <dee_downs@outlook.com>
2020-09-08 19:31:23 -0400

    fixed server GET request

--- api/server.js
+++ api/server.js
@@ -11,9 +11,9 @@ server.use(express.json())
 
 server.use('/api/cars', carsRouter)
 
-server.get('/'), (req, res) => {
+server.get('/', (req, res) => {
     res.status(200).json({ api: 'server upppp'})
-}
+})
 
 function logger(req, res, next) {
     console.log(`a ${req.method} request was made to ${req.url}`)
